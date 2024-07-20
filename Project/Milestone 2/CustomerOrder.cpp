7d452dc1239713b5fee756d19cc3e0b09a918ae6
Kushaal Singh <kna28@myseneca.ca>
2024-07-20 16:09:26 -0400

    Item Struct added

---
+++ Project/Milestone 2/CustomerOrder.cpp
@@ -0,0 +1,7 @@
+#include "CustomerOrder.h"
+
+namespace seneca {
+
+		
+
+}
---
+++ Project/Milestone 2/CustomerOrder.h
@@ -0,0 +1,14 @@
+#ifndef SENECA_CUSTOMER_ORDER_H
+#define SENECA_CUSTOMER_ORDER_H
+
+#include "Station.h"
+
+struct Item {
+	std::string m_itemName;
+	size_t m_serialNumber { 0 };
+	bool m_isFilled { false };
+
+	Item(const std::string& src) : m_itemName(src) {};
+};
+
+#endif
