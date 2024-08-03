27a0c594dcfa88da0a294f6f855041237dd7b145
Kushaal Singh <kna28@myseneca.ca>
2024-08-02 23:07:52 -0400

    Previous files added

---
+++ Project/Milestone3/Utilities.h
@@ -0,0 +1,36 @@
+// Name: Kushaal Singh
+// Seneca Student ID: 115320228
+// Seneca email: kna28@myseneca.ca
+// Date of completion: 13/7/2024
+//
+// I confirm that I am the only author of this file
+//   and the content was created entirely by me.
+
+#ifndef SENECA_UTILITIES_H
+#define SENECA_UTILITIES_H
+
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+namespace seneca {
+	
+	class Utilities {
+	private:
+		size_t m_widthField { 1 };
+		static char m_delimiter;
+		
+	public:
+		void setFieldWidth(size_t newWidth);
+		size_t getFieldWidth() const;
+		std::string extractToken(const std::string& str, size_t& next_pos, bool& more);
+
+		static void setDelimiter(char newDelimiter);
+		static char getDelimiter();
+	};
+
+	std::string trim(const std::string& str);
+	bool emptyStr(const std::string& str);
+}
+
+#endif
