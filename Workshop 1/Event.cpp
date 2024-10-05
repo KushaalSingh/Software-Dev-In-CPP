d11f77a0f9a9ec84c6f61ae4582a0dd6cbb9fb7f
Kushaal Singh <kna28@myseneca.ca>
2024-10-04 22:20:54 -0400

    all formating done

--- Workshop 1/Event.cpp
+++ Workshop 1/Event.cpp
@@ -8,15 +8,18 @@ namespace seneca {
 
 	std::ostream& operator << (std::ostream& out, const Event& src) {
 		static size_t i = 0;
-		out << std::left << std::setw(2) << std::right << ++i <<": " << std::right << std::setw(40) << src.m_name << " -> ";
-
-		/*if (g_settings.m_time_units == "seconds") out << std::setw(2);
-		else if (g_settings.m_time_units == "milliseconds") out << std::setw(5);
-		else if (g_settings.m_time_units == "microseconds") out << std::setw(8);
-		else if (g_settings.m_time_units == "nanoseconds") out << std::setw(11);*/
+		out << std::left << std::setw(2) << std::right << ++i <<": " << std::right << std::setw(40) << src.m_name << " -> " << std::right;
 		
-		out << std::right << std::setw(11) << src.m_duration.count() << " " << g_settings.m_time_units << std::endl;
+		if (g_settings.m_time_units == "seconds") 
+			out << std::setw(2) << std::chrono::duration_cast<std::chrono::seconds>(src.m_duration).count() << " seconds" ;
+		else if (g_settings.m_time_units == "milliseconds")
+			out << std::setw(5) << std::chrono::duration_cast<std::chrono::milliseconds>(src.m_duration).count() << " milliseconds";
+		else if (g_settings.m_time_units == "microseconds")
+			out << std::setw(8) << std::chrono::duration_cast<std::chrono::microseconds>(src.m_duration).count() << " microseconds";
+		else if (g_settings.m_time_units == "nanoseconds")
+			out << std::setw(11) << src.m_duration.count() << " nanoseconds";
 
+		out << std::endl;
 		return out;
 	}
 }
