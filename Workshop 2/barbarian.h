78b618513f279e6f258ca3e010a38d4bcf19f303
Kushaal Singh <kna28@myseneca.ca>
2024-10-17 19:36:47 -0400

    barbarian module completed

--- Workshop 2/barbarian.h
+++ Workshop 2/barbarian.h
@@ -1,1 +1,74 @@
-#include SENECA_BARBARIAN_H
+#ifndef SENECA_BARBARIAN_H
+#define SENECA_BARBARIAN_H
+
+#include "characterTpl.h"
+
+namespace seneca {
+
+	template <typename T, typename Ability_t, typename Weapon_t>
+	class Barbarian : public CharacterTpl<T> {
+		//using Character::getName;
+		int m_baseDefense;
+		int m_baseAttack;
+		Ability_t m_ability;
+		Weapon_t m_weapon[2];
+
+	public:
+		Barbarian(const char* name, int healthMax, int baseAttack, int baseDefense, Weapon_t primaryWeapon, Weapon_t secondaryWeapon);
+		int getAttackAmnt() const;
+		int getDefenseAmnt() const;
+		Character* clone() const;
+		void attack(Character* enemy);
+		void takeDamage(int dmg);
+	};
+
+	template <typename T, typename Ability_t, typename Weapon_t>
+	Barbarian<T, Ability_t, Weapon_t>::Barbarian(const char* name, int healthMax, int baseAttack, int baseDefense, Weapon_t primaryWeapon, Weapon_t secondaryWeapon) : CharacterTpl<T>(name, healthMax), m_baseAttack(baseAttack), m_baseDefense(baseDefense) {
+		m_weapon[0] = primaryWeapon;
+		m_weapon[1] = secondaryWeapon;
+	}
+
+	template <typename T, typename Ability_t, typename Weapon_t>
+	int Barbarian<T, Ability_t, Weapon_t>::getAttackAmnt() const {
+		return (m_baseAttack + (static_cast<double>(m_weapon[0]) / 2) + (static_cast<double>(m_weapon[1]) / 2));
+	}
+
+	template <typename T, typename Ability_t, typename Weapon_t>
+	int Barbarian<T, Ability_t, Weapon_t>::getDefenseAmnt() const {
+		return m_baseDefense;
+	}
+
+	template <typename T, typename Ability_t, typename Weapon_t> 
+	Character* Barbarian<T, Ability_t, Weapon_t>::clone() const {
+		return new Barbarian(*this);
+	}
+
+	template <typename T, typename Ability_t, typename Weapon_t> 
+	void Barbarian<T, Ability_t, Weapon_t>::attack(Character* enemy) {
+		std::cout << getName() << " is attacking " << enemy->getName() << "." << std::endl;
+
+		m_ability.useAbility(*this);
+		auto damage = getAttackAmnt();
+		m_ability.transformDamageDealt(damage);
+
+		std::cout << "Barbarian deals " << damage << " melee damage!" << std::endl;
+		enemy->takeDamage(damage);
+	}
+
+	template <typename T, typename Ability_t, typename Weapon_t>
+	void Barbarian<T, Ability_t, Weapon_t>::takeDamage(int dmg) {
+		std::cout << getName() << " is attacked for " << dmg << " damage." << std::endl;
+		std::cout << "    Barbarian has a defense of " << m_baseDefense << ". Reducing damage received." << std::endl;
+
+		dmg -= m_baseDefense;
+		if (dmg < 0) dmg = 0;
+
+		m_ability.transformDamageReceived(dmg);
+		CharacterTpl<T>::takeDamage(reducedDmg);
+	}
+
+
+}
+
+
+#endif
--- Workshop 2/characterTpl.h
+++ Workshop 2/characterTpl.h
@@ -6,47 +6,54 @@
 namespace seneca {
 	
 	template <typename T>
-	class characterTpl : public Character {
+	class CharacterTpl : public Character {
 		int m_healthMax;
 		T m_health;
 
 	public:
-		characterTpl(std::string name, int maxHealth);
+		CharacterTpl(std::string name, int maxHealth);
 		void takeDamage(int dmg) override;
 		int getHealth() const override;
 		int getHealthMax() const override;
 		void setHealth(int health) override;
 		void setHealthMax(int health) override;
+		const std::string& getName() const override;
 	};
 
 	template <typename T>
-	characterTpl<T>::characterTpl(std::string name, int maxHealth) : Character(name), m_healthMax(maxHealth), m_health(maxHealth) {}
+	CharacterTpl<T>::CharacterTpl(std::string name, int maxHealth) : Character(name), m_healthMax(maxHealth), m_health(maxHealth) {}
 
 	template <typename T>
-	void characterTpl<T>::takeDamage(int dmg) {
+	void CharacterTpl<T>::takeDamage(int dmg) {
 		m_health -= dmg;
 		if (m_health <= 0) std::cout << getName() << "has been defeated!" << std::endl;
 		else std::cout << getName() << " took " << dmg << " damage ," << m_health << "health remaining." << std::endl;
 	}
 
 	template <typename T> 
-	int characterTpl<T>::getHealth() const {
+	int CharacterTpl<T>::getHealth() const {
 		return static_cast<int>(m_health);
 	}
 
 	template <typename T>
-	int characterTpl<T>::getHealthMax() const {
+	int CharacterTpl<T>::getHealthMax() const {
 		return m_healthMax;
 	}
 
 	template <typename T>
-	void characterTpl<T>::setHealth(int health) {
+	void CharacterTpl<T>::setHealth(int health) {
 		m_health = health;
 	}
 
 	template <typename T>
-	void characterTpl<T>::setHealthMax(int health) {
+	void CharacterTpl<T>::setHealthMax(int health) {
 		m_healthMax = health;
+		m_health = health;
+	}
+
+	template <typename T>
+	const std::string& CharacterTpl<T>::getName() const {
+		return Character::getName();
 	}
 }
 
