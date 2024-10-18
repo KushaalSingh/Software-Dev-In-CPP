b64d7ee24de96772f6734161c93858c3521a90fe
Kushaal Singh <kna28@myseneca.ca>
2024-10-17 22:48:11 -0400

    module implemented

---
+++ Workshop 2/team.cpp
@@ -0,0 +1,105 @@
+#include "team.h"
+
+namespace seneca {
+
+	Team::Team() : m_characters(nullptr), m_name(""), m_size(0) {}
+
+	Team::Team(const char* name) : m_characters(nullptr), m_name(name), m_size(0) {}
+
+	Team::Team(const Team& src) : m_characters(nullptr), m_name(src.m_name), m_size(src.m_size) {
+		m_characters = new Character * [m_size];
+		for (size_t i = 0; i < m_size; i++) m_characters[i] = src.m_characters[i]->clone();
+	}
+
+	Team& Team::operator = (const Team& src) {
+		if (this != &src) {
+			for (size_t i = 0; i < m_size; i++) delete m_characters[i];
+			delete[] m_characters;
+
+			m_size = src.m_size;
+			m_name = src.m_name;
+
+			m_characters = new Character * [m_size];
+			for (size_t i = 0; i < m_size; i++) m_characters[i] = src.m_characters[i]->clone();
+		}
+		return *this;
+	}
+
+	Team::Team(Team&& src) noexcept : m_characters(src.m_characters), m_name(src.m_name), m_size(src.m_size) {
+		src.m_characters = nullptr;
+		src.m_name = "";
+		src.m_size = 0;
+	}
+
+	Team& Team::operator = (Team&& src) noexcept {
+		if (this != &src) {
+			for (size_t i = 0; i < m_size; i++) delete m_characters[i];
+			delete[] m_characters;
+
+			m_characters = src.m_characters;
+			m_name = src.m_name;
+			m_size = src.m_size;
+
+			src.m_characters = nullptr;
+			src.m_name = "";
+			src.m_size = 0;
+		}
+		return *this;
+	}
+
+	Team::~Team() {
+		for (size_t i = 0; i < m_size; i++) delete m_characters[i];
+		delete[] m_characters;
+		m_characters = nullptr;
+	}
+
+	void Team::addMember(const Character* c) {
+		bool found = false;
+		for (size_t i = 0; i < m_size; i++) if (m_characters[i]->getName() == c->getName()) found = true;
+
+		if (!found) {
+			Character** temp = new Character * [m_size + 1];
+			for (size_t i = 0; i < m_size; i++) temp[i] = m_characters[i]->clone();
+			temp[m_size] = c->clone();
+			delete[] m_characters;
+			m_characters = temp;
+			++m_size;
+		}
+	}
+
+	void Team::removeMember(const std::string& c) {
+		size_t index = 0;
+		for (size_t i = 0; i < m_size; i++) if (m_characters[i]->getName() == c) index = i;
+
+		size_t index = m_size;
+		for (size_t i = 0; i < m_size; ++i) {
+			if (m_characters[i]->getName() == c) {
+				index = i;
+				break;
+			}
+		}
+
+		delete m_characters[index];
+
+		Character** temp = new Character * [m_size - 1];
+		for (size_t i = 0, k = 0; i < m_size; ++i) {
+			if (i != index) {
+				temp[k++] = m_characters[i];
+			}
+		}
+
+		delete[] m_characters;
+		m_characters = temp;
+		--m_size;
+	}
+
+	Character* Team::operator[](size_t idx) const {
+		if (idx < m_size) return m_characters[idx];
+		return nullptr;
+	}
+
+	void Team::showMembers() const {
+		std::cout << "[Team] " << m_name << std::endl;
+		for (size_t i = 0; i < m_size; i++) std::cout << std::right << std::setw(5) << i << ": " << *m_characters[i] << std::endl;
+	}
+}
---
+++ Workshop 2/team.h
@@ -0,0 +1,31 @@
+#ifndef SENECA_TEAM_H
+#define SENECA_TEAM_H
+
+#include "character.h"
+
+namespace seneca {
+
+	
+	class Team {
+		Character** m_characters;
+		std::string m_name;
+		size_t m_size;
+
+	public:
+		Team();
+		Team(const char* name);
+		Team(const Team& src);
+		Team& operator = (const Team& src);
+		Team(Team&& src) noexcept;
+		Team& operator = (Team&& src) noexcept;
+		~Team();
+		void addMember(const Character* c);
+		void removeMember(const std::string& c);
+		Character* operator[](size_t idx) const;
+		void showMembers() const;
+	};
+
+}
+
+
+#endif
