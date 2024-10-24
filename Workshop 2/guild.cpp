6055b64c00058427fc68160599572f251aadc4a6
Kushaal Singh <kna28@myseneca.ca>
2024-10-23 23:12:31 -0400

    memory leak problem

--- Workshop 2/guild.cpp
+++ Workshop 2/guild.cpp
@@ -8,18 +8,19 @@ namespace seneca {
 
 	Guild::Guild(const Guild& src) : m_characters(nullptr), m_name(src.m_name), m_size(src.m_size) {
 		m_characters = new Character * [m_size];
-		for (size_t i = 0; i < m_size; i++) m_characters[i] = src.m_characters[i];
+		for (size_t i = 0; i < m_size; i++) m_characters[i] = src.m_characters[i]->clone();
 	}
 
 	Guild& Guild::operator = (const Guild& src) {
 		if (this != &src) {
+			for (size_t i = 0; i < m_size; i++) delete m_characters[i];
 			delete[] m_characters;
 
 			m_size = src.m_size;
 			m_name = src.m_name;
 
 			m_characters = new Character * [m_size];
-			for (size_t i = 0; i < m_size; i++) m_characters[i] = src.m_characters[i];
+			for (size_t i = 0; i < m_size; i++) m_characters[i] = src.m_characters[i]->clone();
 		}
 		return *this;
 	}
@@ -32,6 +33,7 @@ namespace seneca {
 
 	Guild& Guild::operator = (Guild&& src) noexcept {
 		if (this != &src) {
+			for (size_t i = 0; i < m_size; i++) delete m_characters[i];
 			delete[] m_characters;
 
 			m_characters = src.m_characters;
@@ -46,6 +48,7 @@ namespace seneca {
 	}
 
 	Guild::~Guild() {
+		for (size_t i = 0; i < m_size; i++) delete m_characters[i];
 		delete[] m_characters;
 		m_characters = nullptr;
 	}
@@ -79,6 +82,7 @@ namespace seneca {
 		if (index == m_size) return;
 
 		m_characters[index]->setHealthMax(m_characters[index]->getHealthMax() - 300);
+		delete m_characters[index];
 		Character** temp = new Character * [m_size - 1];
 
 		for (size_t i = 0, k = 0; i < m_size; i++) if (i != index) temp[k++] = m_characters[i];
