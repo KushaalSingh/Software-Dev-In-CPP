4cd102853da01ca43de40a8b194c2531cb045e8c
Kushaal Singh <kna28@myseneca.ca>
2024-10-23 22:57:15 -0400

    health made to updates to zero now

--- Workshop 2/characterTpl.h
+++ Workshop 2/characterTpl.h
@@ -27,7 +27,10 @@ namespace seneca {
 	template <typename T>
 	void CharacterTpl<T>::takeDamage(int dmg) {
 		m_health -= dmg;
-		if (m_health <= 0) std::cout << "    " << getName() << " has been defeated!" << std::endl;
+		if (m_health <= 0) {
+			m_health = 0;
+			std::cout << "    " << getName() << " has been defeated!" << std::endl;
+		}
 		else std::cout << "    " << getName() << " took " << dmg << " damage, " << m_health << " health remaining." << std::endl;
 	}
 
